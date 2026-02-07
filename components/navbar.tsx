d7e44d8872591f82bb6f17af0fac7d0fc1c9f511
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-02-07 23:53:10 +0530

    added job-tracker log-in and sign-up buttons in the navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,35 @@
 import { Briefcase } from "lucide-react";
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 export default function Navbar(){
     return (
-        <nav className="border-b  border-gray-200 bg-white">
-            <div className="container mx-auto flex h-16 items-center px-4  ">
-                <Link href="/" className="flex items-center gap-2 text-xl font-semibold text-primary">
+        <nav className="border-b border-gray-200 bg-white  ">
+            <div className="container mx-auto flex h-16 items-center px-4 justify-between">
+                <Link 
+                    href="/" 
+                    className="flex items-center gap-2 text-xl font-semibold text-primary"
+                >
                 <Briefcase />
                 Job Tracker
                 </Link>
+            <div>
+                <Link href="/sign-in" >
+                    <Button 
+                        variant="ghost" 
+                        className="text-gray-700 hover:text-black"
+                    >
+                        Log In
+                    </Button>
+                </Link>
+                <Link href="/sign-up">
+                    <Button className="bg-primary hover:bg-primary/90">
+                        Start for free
+                    </Button>
+                </Link>
+            </div>
             </div>
         </nav>
     );
 }
+
