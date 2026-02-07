4c2a259dbcb73a2707f46927e1d6b2981dfde4c6
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-02-07 23:23:33 +0530

    linked pages

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,14 @@
 import { Briefcase } from "lucide-react";
+import Link from "next/link";
 
 export default function Navbar(){
     return (
         <nav>
             <div>
+                <Link href="/" className="flex items-center gap-2 text-xl font-semibold text-primary">
                 <Briefcase />
+                Job Tracker
+                </Link>
             </div>
         </nav>
     );
