85716a1473a6f82a0d99f6a63735f3dc5246731a
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-02-08 04:31:54 +0530

    using mongodb for database

---
+++ lib/db.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose"
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if(!MONGODB_URI) {
+    throw new Error(
+        "Please define the MONGODB_URI enviornment variable inside .env"
+    );
+}
+
