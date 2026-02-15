4cfe742399e6fab3fe844aeef4489cbecdea7cb2
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-02-16 03:13:55 +0530

    board.ts

--- lib/models/board.ts
+++ lib/models/board.ts
@@ -1,5 +1,35 @@
 import mongoose, {Schema, Document} from "mongoose";
 
 export interface Iboard extends Document{
-    text
+    name: string;
+    userId: string;
+    columns: mongoose.Types.ObjectId[];
+    createdAt: Date;
+    updatedAt: Date;
 }
+
+const BoardSchema = new Schema<Iboard>({
+    name: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: String,
+        required: true,
+        index: true
+    },
+    columns: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Column",
+        },
+    ],
+
+},
+{
+    timestamps: true,
+}
+
+);
+
+export default mongoose.models.Board || mongoose.model<Iboard>("Board", BoardSchema);
