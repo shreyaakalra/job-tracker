e77ed3fd5dcbac45efbd10af0309de877819dadb
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-02-16 23:45:51 +0530

    user board

--- lib/init-user-board.ts
+++ lib/init-user-board.ts
@@ -1,1 +1,52 @@
 import connectDB from "./db"
+import {Board, Column} from "./models"
+
+const DEFAULT_COLUMNS = [
+    {
+        name: "Wish List",
+        order: 0,
+    },
+    {
+        name: "Applied",
+        order: 1,
+    },
+    {
+        name: "Interviewing",
+        order: 2,
+    },
+    {
+        name: "Offer",
+        order: 3,
+    },
+    {
+        name: "Rejected",
+        order: 4,
+    },
+
+]
+
+export async function initializeUserBoard(userId: string){
+    try {
+        await connectDB();
+
+        // check if the board already exists
+        const existingBoard = await Board.findOne({userId: userId, name: "Job Hunt"});
+
+        if(existingBoard){
+            return existingBoard;
+        }
+
+        // create the board
+        const board = await Board.create({
+            name: "Job Hunt",
+            userId,
+            columns: []
+        });
+
+        // create default columns
+        
+
+    } catch (err) {
+        throw err;
+    }
+}
