34137645e39b03d66fdee1e77f7116d6109e786e
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-02-18 03:55:45 +0530

    using proxy server to redirect to sign in page

--- proxy.ts
+++ proxy.ts
@@ -3,8 +3,12 @@ import { getSession } from "./lib/auth/auth";
 
 export default async function proxy(request: NextRequest){
     const session = await getSession();
-    if(!session?.user){
+
+    const isDashboardPage = request.nextUrl.pathname.startsWith("/dashboard");
+
+    if(isDashboardPage && !session?.user){
         return NextResponse.redirect(new URL("/sign-in", request.url));
     }
 
+    return NextResponse.next();
 }
