0cc069c729937f34121175770e58c201c9eabbbb
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-02-18 04:33:55 +0530

    another method to not let users access dashboard without them being signed in

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,15 @@
-export default function Dashboard(){
+import { getSession } from "@/lib/auth/auth";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard(){
+
+    const session = await getSession();
+
+    if(!session?.user){
+        redirect("/sign-in")
+    }
+
+
     return(
         <div>
             DASHBOARD
--- proxy.ts
+++ proxy.ts
@@ -3,11 +3,13 @@ import { getSession } from "./lib/auth/auth";
 
 export default async function proxy(request: NextRequest){
     const session = await getSession();
+    
+    const isSignInPage = request.nextUrl.pathname.startsWith("/sign-in");
 
-    const isDashboardPage = request.nextUrl.pathname.startsWith("/dashboard");
+    const isSignUpPage = request.nextUrl.pathname.startsWith("/sign-up");
 
-    if(isDashboardPage && !session?.user){
-        return NextResponse.redirect(new URL("/sign-in", request.url));
+    if((isSignInPage || isSignUpPage) && session?.user){
+        return NextResponse.redirect(new URL("/dashboard", request.url));
     }
 
     return NextResponse.next();
