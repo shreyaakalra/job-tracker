fb9be386f6d92af0ff480cddc86900398e7e4480
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-03-20 03:42:21 +0530

    baseurl

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -9,6 +9,8 @@ const client = new MongoClient(process.env.MONGODB_URI!);
 const db = client.db();
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL || `https://${process.env.VERCEL_URL}`,
+  
   database: mongodbAdapter(db, {
     client,
   }),
